efedd4b13cbb9ba01eab6c63495a8efbe5486596
yang24201 <yang24201@126.com>
2016-05-04 01:14:37 +0800

    修改主页，添加后台主页

---
+++ www/backend/index.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>梅女王的后台</title>
+		<link rel="stylesheet" type="text/css" href="../css/comm.css">
+		<link rel="stylesheet" type="text/css" href="../css/backend.css">
+	</head>
+	<body>
+		<div class="container">
+			<div id="header" class="header">
+				<div class="func_area">
+					<a href="" class="super button green">新建文章</a>
+					<a href="" class="super button green">添加书签</a>
+					<a href="" class="super button green">删除书签</a>
+					<a href="" class="super button green">主页设置</a>
+					<a href="" class="super button red">预览主页</a>
+				<div>
+			</div>
+			<div id="main_list" class="main_list">
+			</div>
+			<div class="footer">
+			</div>
+			<div class="go_to_top" title="回到顶部">
+				<a href="#main_list" >
+					<img src="../img/go_back.png" />
+				</a>
+			</div>
+		</div>
+	</body>
+</html>
